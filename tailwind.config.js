c02133357a23df787d845ef83faffd884f0c778c
Mustafa-Alarify <m2024u775516215@gmail.com>
2025-06-13 14:30:21 +0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+    colors: {
+      primaryDialog: "#4278ff",
+    }
+  },
+  plugins: [],
+}
+
